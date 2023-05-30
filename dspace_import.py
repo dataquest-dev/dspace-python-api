427fb725a91a366fe6fb356b9fbdba0c79889bc4
Paurikova2 <michaela.paurikova@dataquest.sk>
2023-05-30 18:59:30 +0200

    try to do rebase

--- dspace_import.py
+++ dspace_import.py
@@ -909,6 +909,50 @@ def import_handle_with_url():
 
     print("Handles with url were successfully imported!")
 
+<<<<<<< HEAD
+=======
+def import_user_metadata():
+    """
+    Import data into database.
+    Mapped tables: user_metadata, license_resource_user_allowance
+    """
+    global eperson_id, bitstream_id
+    #read license_resource_user_allowance
+    #mapping eperson_id to mapping_id
+    user_allowance = dict()
+    json_a = read_json("license_resource_user_allowance.json")
+    if json_a:
+        for i in json_a:
+            if i['eperson_id'] in user_allowance:
+                user_allowance[i['eperson_id']].append(i)
+            else:
+                user_allowance[i['eperson_id']] = [i]
+
+    #read license_resource_mapping
+    #mapping bitstream_id to mapping_id
+    resource_mapping = read_json('license_resource_mapping.json')
+    mappings = dict()
+    if resource_mapping:
+        for i in resource_mapping:
+            mappings[i['mapping_id']] = i['bitstream_id']
+
+    #read user_metadata
+    json_a = read_json("user_metadata.json")
+    if json_a:
+        for i in json_a:
+            if i['eperson_id'] in user_allowance:
+                dataUA = user_allowance[i['eperson_id']]
+                for data in dataUA:
+                    json_p = [{'metadataKey': i['metadata_key'], 'metadataValue': i['metadata_value']}]
+                    #bitstream_id[mappings[data['mapping_id']]]
+                    param = {'bitstreamUUID': bitstream_id[870], 'epersonId': eperson_id[i['eperson_id']],
+                             'createdOn': data['created_on'], 'token': data['token']}
+                    try:
+                        do_api_post('clarin/import/usermetadata', param, json_p)
+                    except:
+                        log('POST response clarin/import/usermetadata failed for eperson_id: ' + str(i['eperson_id'])
+                            + ' and bitstream id: ' + str(mappings[data['mapping_id']]))
+>>>>>>> ffb0ea7 (set object to array of objects)
 
 def import_epersons_and_groups():
     """
@@ -962,4 +1006,9 @@ import_hierarchy()
 import_epersons_and_groups()
 import_licenses()
 import_bundles_and_bitstreams()
+<<<<<<< HEAD
 print("Data migration is completed!")
+=======
+#import_user_metadata()
+print("Data migration is completed!")
+>>>>>>> ffb0ea7 (set object to array of objects)
