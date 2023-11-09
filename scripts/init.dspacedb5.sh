7af3cea09dac1054fec6faccaad137b2cedd2954
milanmajchrak <milan.majchrak@dataquest.sk>
2023-11-09 14:50:05 +0100

    Add result of importing logs into files because in the `postgres.log` are only errors of the first attempt and the user do not know that the dumps was successfully imported.

--- scripts/init.dspacedb5.sh
+++ scripts/init.dspacedb5.sh
@@ -8,11 +8,11 @@ createuser --username=postgres dspace
 
 echo "Importing clarin-dspace"
 createdb --username=postgres --owner=dspace --encoding=UNICODE clarin-dspace
-psql -U postgres clarin-dspace < ../dump/clarin-dspace-8.8.23.sql &> /dev/null
+psql -U postgres clarin-dspace < ../dump/clarin-dspace-8.8.23.sql &> ./clarin-dspace.log
 
 echo "Importing clarin-utilities"
 createdb --username=postgres --encoding=UNICODE clarin-utilities
-psql -U postgres clarin-utilities < ../dump/clarin-utilities-8.8.23.sql &> /dev/null
+psql -U postgres clarin-utilities < ../dump/clarin-utilities-8.8.23.sql &> ./clarin-utilities.log
 
 echo "Done, starting psql"
 
