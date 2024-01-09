053c0a6173474682a76edf80aca72303183e910a
milanmajchrak <milan.majchrak@dataquest.sk>
2024-01-09 14:38:31 +0100

    Updated `create_bitstream` method because multipart file was uploaded without content.

--- support/dspace_interface/client.py
+++ support/dspace_interface/client.py
@@ -24,7 +24,7 @@ import os
 from uuid import UUID
 
 import requests
-from requests import Request
+from requests_toolbelt import MultipartEncoder
 
 from support.dspace_interface.response_map import check_response
 from .models import DSpaceObject, SimpleDSpaceObject, Bundle, Bitstream, Community, \
@@ -606,7 +606,7 @@ class DSpaceClient:
             return None
         url = f'{self.API_ENDPOINT}core/items/{parent.uuid}/bundles'
         return Bundle(api_resource=parse_json(
-            self.api_post(url, params=None, json_p={'name': name, 'metadata': {}})))
+            self.api_post(url, params=None, data={'name': name, 'metadata': {}})))
 
     def get_bitstreams(self, uuid=None, bundle=None, page=0, size=20):
         """
@@ -671,9 +671,21 @@ class DSpaceClient:
         payload = {'properties': json.dumps(properties) + ';application/json'}
         h = self.session.headers
         h.update({'Content-Encoding': 'gzip'})
-        req = Request('POST', url, data=payload, headers=h, files=files)
-        prepared_req = self.session.prepare_request(req)
-        r = self.session.send(prepared_req)
+
+        mp_encoder = MultipartEncoder(
+            fields={
+                'file': (name, open(path, 'rb'), 'text/plain'),
+            }
+        )
+        h.update({'Content-Type': mp_encoder.content_type})
+        r = self.session.post(
+            url,
+            # The MultipartEncoder is posted as data, don't use files=...!
+            data=mp_encoder,
+            # The MultipartEncoder provides the content-type header with the boundary:
+            headers=h
+        )
+
         if 'DSPACE-XSRF-TOKEN' in r.headers:
             t = r.headers['DSPACE-XSRF-TOKEN']
             logging.debug('Updating token to ' + t)
