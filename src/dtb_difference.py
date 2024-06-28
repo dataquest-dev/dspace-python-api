6dc79e5d21cd0c7218a2f53f397f6595213a592b
Paurikova2 <michaela.paurikova@dataquest.sk>
2024-06-28 11:43:30 +0200

    more clear result

--- src/dtb_difference.py
+++ src/dtb_difference.py
@@ -15,28 +15,29 @@ import pump
 
 def difference_dtb(old_dtb: dict, new_dtb: dict):
     msg = ""
-    no_exist7 = ""
-    no_exist5 = ""
+    no_exist7 = []
+    no_exist5 = []
     for name in sorted(old_dtb.keys()):
         if name not in new_dtb:
-            no_exist7 += f"{name},"
+            no_exist7.append(name)
         else:
             difference = int(new_dtb[name]) - int(old_dtb[name])
             result = "surplus " if difference > 0 else (
                 "deficit " if difference < 0 else "")
             msg += f"{name: >40}: {int(difference): >8d} {result}\n"
-        del new_dtb[name]
+            del new_dtb[name]
     for name in sorted(new_dtb.keys()):
-        no_exist5 += f"{name},"
+        no_exist5.append(name)
     _logger.info(
-        f"\n{msg}Nonexistent tables in DSpace 7:\n\t{no_exist7}\nNonexistent tables in DSpace 5:\n\t{no_exist5}")
+        f"\n{msg}Nonexistent tables in DSpace 7: {', '.join(no_exist7)}\nCount: {len(no_exist7)}"
+        f"\nNonexistent tables in DSpace 5: {', '.join(no_exist5)}\nCount: {len(no_exist5)}")
     _logger.info(40 * "=")
 
 
 if __name__ == "__main__":
     _logger.info("Loading repo objects")
 
-    _logger.info("Database difference:")
+    _logger.info("Database difference (v6 vs v7):")
     raw_db_7 = pump.db(env["db_dspace_7"])
     raw_db_tul = pump.db(env["db_tul"])
     difference_dtb(raw_db_tul.table_count(), raw_db_7.table_count())
